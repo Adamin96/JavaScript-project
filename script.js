229e27e89b6d9c857d2fbbce979130624fc5ffdd
Egor Volkov <adamin96@mail.ru>
2022-02-25 00:00:11 +0700

    create: new lesson02

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = "JavaScript-project";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 48;
+const rollback = 58;
+const fullPrice = 9000;
+const adaptive = true;
 
+console.log(typeof title, fullPrice, adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов", screenPrice, "рублей");
+console.log("Стоимость разработки сайта", fullPrice, "рублей");
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+console.log(fullPrice * (rollback / 100), "рублей");
 console.log("Hi, everybody");
 
 alert("Welcome");
