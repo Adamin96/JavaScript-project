8b5851fb9fb9d1ebad53b5a2d659d71d215d7049
Egor Volkov <adamin96@mail.ru>
2022-03-21 00:10:31 +0700

    create: lesson03

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-const title = "JavaScript-project";
-const screens = "Простые, Сложные, Интерактивные";
-const screenPrice = 48;
-const rollback = 58;
-const fullPrice = 9000;
-const adaptive = true;
+const title = prompt("Как назвается ваш проект?");
 
-console.log(typeof title, fullPrice, adaptive);
-console.log(screens.length);
-console.log("Стоимость верстки экранов", screenPrice, "рублей");
-console.log("Стоимость разработки сайта", fullPrice, "рублей");
-console.log(screens.toLowerCase());
-console.log(screens.split(", "));
-console.log(fullPrice * (rollback / 100), "рублей");
-console.log("Hi, everybody");
+const screens = prompt(
+  "Какие типы экранов нужно разработать? Простые, Сложные, Интерактивные?"
+);
 
-alert("Welcome");
+const screenPrice = +prompt("Сколько будет стоить данная работа?");
+
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+
+const service1 = prompt("Какой дополнительный тип услуги №1 нужен?");
+const servicePrice1 = +prompt("Сколько это будет стоить?");
+
+const service2 = prompt("Какой дополнительный тип услуги №2 нужен?");
+const servicePrice2 = +prompt("Сколько это будет стоить?");
+
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+// Скидка
+let discount;
+
+if (fullPrice >= 30000) {
+  discount = "Даем скидку в 10%";
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+  discount = "Даем скидку в 5%";
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+  discount = "Скидка не предусмотрена";
+} else {
+  discount = "Что то пошло не так";
+}
+
+const rollback = 10;
+
+const rollbackPrice = fullPrice * (rollback / 100);
+
+const servicePercentPrice = fullPrice - rollbackPrice;
+
+console.log(typeof title, title);
+console.log(typeof screens, screens);
+console.log(typeof screenPrice, screenPrice);
+console.log(typeof adaptive, adaptive);
+console.log(typeof service1, service1);
+console.log(typeof servicePrice1, servicePrice1);
+console.log(typeof service2, service2);
+console.log(typeof servicePrice2, servicePrice2);
+console.log(typeof fullPrice, "итоговая стоимость работы =", fullPrice);
+console.log(typeof rollbackPrice, "откат посреднику =", rollbackPrice);
+console.log(
+  typeof servicePercentPrice,
+  "итоговая стоимость за вычетом отката посреднику =",
+  servicePercentPrice
+);
+console.log(discount);
+alert(discount);
